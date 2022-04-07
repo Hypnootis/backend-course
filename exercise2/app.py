8cbbbb8f94bdf9fcb07f76f43e42dcbc9fb2bcbb
hypnootis <tm.pasanen@outlook.com>
2022-04-07 12:45:00 +0300

    exercise2

--- exercise2/app.py
+++ exercise2/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, make_response, render_template, url_for
+from flask import Flask, request, jsonify, make_response, render_template, redirect, url_for
 app = Flask(__name__, template_folder="static")
 
 all_items = []
@@ -25,12 +25,15 @@ allstudents = [
 
 @app.route("/students", methods=["GET", "POST"])
 def students():
-    request_data = request.get_json()
-    if request_data:
-        allstudents.append(request_data)
-        return make_response(jsonify(request_data), 200)
+    if request.method == "POST":
+        request_data = request.get_json()
+        if request_data:
+            allstudents.append(request_data)
+            return make_response(jsonify(request_data), 200)
+        else:
+            return "Request must contain data in json format", 400
     else:
-        return "Request must contain data in json format", 400
+        return make_response(jsonify(allstudents), 200)
 
 @app.route("/students/<degree>", methods=["GET"])
 def degrees(degree):
@@ -43,20 +46,57 @@ def degrees(degree):
     else:
         return "No students matching this degree"
 
-@app.route("/", methods=["GET", "POST"])
-def forms():
+@app.route("/students/<int:snumber>", methods=["PUT"])
+def replace_student(snumber):
+    student_exists = False
+    request_data = request.get_json()
+    for student in allstudents:
+        if student["student_number"] == snumber:
+            student = request_data
+            student_exists = True
+            return make_response({"message": "Student replaced"}, 200)
+
+    if not student_exists:
+        allstudents.append(request_data)
+        return make_response({"message": "Collection created"}, 201)
+
+@app.route("/students/<int:snumber>", methods=["PATCH"])
+def update_student(snumber):
+    student_exists = False
+    request_data = request.get_json()
+    for student in allstudents:
+        if student["student_number"] == snumber:
+            for key in student:
+                student[key] = request_data[key]
+            student_exists = True
+            return make_response({"message": "Student updated"}, 200)
+
+    if not student_exists:
+        allstudents.append(request_data)
+        return make_response({"message": "Student added"}, 201)
+
+@app.route("/students/<int:snumber>", methods=["DELETE"])
+def delete_student(snumber):
+    deleted_student = False
+    for student in allstudents:
+        if student["student_number"] == snumber:
+            allstudents.remove(student)
+            deleted_student = True
+            return make_response({"message": "Student removed"}, 200)
+    if not deleted_student:
+        return make_response({"message": "Student not in list"}, 400)
+
+@app.route("/add", methods=["GET", "POST"])
+def add_student():
     if request.method == "POST":
-        new_item = request.form
-        if new_item not in all_items:
-            for key in new_item:
-                if new_item[key] == "":
-                    return f"Item was not added. Item must have a {key}."
-                    break
-                else:
-                    all_items.append(new_item)
-        return jsonify(all_items)
-    else:
-        return render_template("index.html")
+        res = request.form
+        allstudents.append(res)
+    return render_template("addStudentsForm.html")
+
+
+@app.route("/", methods=["GET", "POST"])
+def main():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.debug = True

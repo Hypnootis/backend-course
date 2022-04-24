4862673bcbec01d47a1f4ee7cf33838db1954fa1
hypnootis <tm.pasanen@outlook.com>
2022-04-24 16:50:14 +0300

    Nearly done with exercise3

--- exercise3/app.py
+++ exercise3/app.py
@@ -2,9 +2,29 @@ from flask import Flask, jsonify, make_response, render_template, url_for
 
 app = Flask(__name__, template_folder="templates")
 
+students = [
+    {"student_number": 123123,
+        "name": "Alice Brown",
+        "credits": 130,
+        "degree": "it"},
+    {"student_number": 111222,
+        "name": "Bob Jones",
+        "credits": 157,
+        "degree": "it"},
+    {"student_number": 333444,
+        "name": "Richard Brown",
+        "credits": 57,
+        "degree": "it"}
+]
+
 @app.route("/")
+@app.route("/home")
 def index():
-    return render_template("home.html")
+    return render_template("home.html", my_students=students)
+
+@app.route("/add")
+def add():
+    return render_template("add.html")
 
 if __name__ == "__main__":
     app.debug = True
